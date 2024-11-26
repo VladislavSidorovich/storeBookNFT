a0f7698ed60fc04ceed70330ade19d2f3fd0cd90
VladislavSidorovich <sidinfo640@gmail.com>
2024-11-27 01:20:49 +0300

    feat: Update CustomSliderСlosedSection.tsx

--- components/CustomSliderСlosedSection.tsx
+++ components/CustomSliderСlosedSection.tsx
@@ -157,7 +157,7 @@ function Index() {
         combinedData.map((el, index) => (
           <SwiperSlide
             key={index}
-            className={index === 0 || index === 1 || index === 3 || index === 4 || index === 5 || index === 6 || index === 7 || index === 8 ? "hidden-slide" : ""}
+            className={index === 2 || index === 1 || index === 3 || index === 4 || index === 5 || index === 6 || index === 7 || index === 8 ? "hidden-slide" : ""}
           >
             {"result" in el ? (
               <SliderItem
